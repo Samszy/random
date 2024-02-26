faf24e35501c819cefcd1bbf132a0b5ab4268c5e
Sammy <57487846+Samszy@users.noreply.github.com>
2024-02-26 03:45:11 -0800

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,13 @@
+let favFood ="Pizza", favColor ="Red", favDrink ="Lemonade";
+
+console.log(favDrink);
+
+favFood ="Pasta";
+
+console.log(favFood);
+
+let myBirth ="October 17th, 1987";
+
+console.log(myBirth);
+
+console.log(100 > 1);
